2d9a50d554264fafb68b714a0c5dac00d1f5013a
Wei Yen Lee <hello@weiyen.net>
2024-10-30 20:37:30 +1100

    Index page updates

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,16 +10,12 @@ import { getAllArticles } from "~/lib/articles.server";
 import { formatDate } from "~/lib/formatDate";
 import type { Article } from "~/lib/types";
 import { ArticleZ } from "~/lib/types";
-import logoAirbnb from "~/images/logos/airbnb.svg";
-import logoFacebook from "~/images/logos/facebook.svg";
-import logoPlanetaria from "~/images/logos/planetaria.svg";
-import logoStarbucks from "~/images/logos/starbucks.svg";
 
 const LoaderDataZ = z.object({ articles: ArticleZ.array() });
 type LoaderData = z.infer<typeof LoaderDataZ>;
 
 export async function loader(): Promise<LoaderData> {
-  const articles = (await getAllArticles()).slice(0, 4);
+  const articles = (await getAllArticles()).slice(0, 10);
   return { articles };
 }
 
@@ -192,105 +188,6 @@ function Newsletter() {
   );
 }
 
-interface Role {
-  company: string;
-  title: string;
-  logo: string;
-  start: string | { label: string; dateTime: string };
-  end: string | { label: string; dateTime: string };
-}
-
-function Role({ role }: { role: Role }) {
-  const startLabel =
-    typeof role.start === "string" ? role.start : role.start.label;
-  const startDate =
-    typeof role.start === "string" ? role.start : role.start.dateTime;
-
-  const endLabel = typeof role.end === "string" ? role.end : role.end.label;
-  const endDate = typeof role.end === "string" ? role.end : role.end.dateTime;
-
-  return (
-    <li className="flex gap-4">
-      <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-        <img src={role.logo} alt="" className="h-7 w-7" />
-      </div>
-      <dl className="flex flex-auto flex-wrap gap-x-2">
-        <dt className="sr-only">Company</dt>
-        <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
-          {role.company}
-        </dd>
-        <dt className="sr-only">Role</dt>
-        <dd className="text-xs text-zinc-500 dark:text-zinc-400">
-          {role.title}
-        </dd>
-        <dt className="sr-only">Date</dt>
-        <dd
-          className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
-          aria-label={`${startLabel} until ${endLabel}`}
-        >
-          <time dateTime={startDate}>{startLabel}</time>{" "}
-          <span aria-hidden="true">—</span>{" "}
-          <time dateTime={endDate}>{endLabel}</time>
-        </dd>
-      </dl>
-    </li>
-  );
-}
-
-function Resume() {
-  const resume: Array<Role> = [
-    {
-      company: "Planetaria",
-      title: "CEO",
-      logo: logoPlanetaria,
-      start: "2019",
-      end: {
-        label: "Present",
-        dateTime: new Date().getFullYear().toString(),
-      },
-    },
-    {
-      company: "Airbnb",
-      title: "Product Designer",
-      logo: logoAirbnb,
-      start: "2014",
-      end: "2019",
-    },
-    {
-      company: "Facebook",
-      title: "iOS Software Engineer",
-      logo: logoFacebook,
-      start: "2011",
-      end: "2014",
-    },
-    {
-      company: "Starbucks",
-      title: "Shift Supervisor",
-      logo: logoStarbucks,
-      start: "2008",
-      end: "2011",
-    },
-  ];
-
-  return (
-    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Work</span>
-      </h2>
-      <ol className="mt-6 space-y-4">
-        {resume.map((role, roleIndex) => (
-          <Role key={roleIndex} role={role} />
-        ))}
-      </ol>
-      <ButtonLink to="#" variant="secondary" className="group mt-6 w-full">
-        Download CV
-        <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-      </ButtonLink>
-    </div>
-  );
-}
-
 export default function Home() {
   const data = useLoaderData();
   const { articles } = LoaderDataZ.parse(data);
@@ -304,7 +201,7 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-10 lg:pl-16 xl:pl-24">
           <Profile />
-          <Newsletter />
+          {/* <Newsletter /> */}
         </div>
       </div>
     </Container>
