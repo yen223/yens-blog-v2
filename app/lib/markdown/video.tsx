38b26950ac9161801caca13f0c5bf08234e7650c
Wei Yen Lee <hello@weiyen.net>
2024-11-08 15:53:56 +1100

    Fix video tag

--- app/lib/markdown/video.tsx
+++ app/lib/markdown/video.tsx
@@ -1,14 +1,19 @@
 type VideoProps = {
-    url: string;
+    src: string;
+    controls?: boolean;
+    className?: string;
 };
 
-export function Video({ url }: VideoProps) {
+
+export function Video({ src, controls = true, className }: VideoProps) {
     return (
-        <video src={url} controls></video>
+        <div className={"flex flex-row justify-center"}>
+            <video src={src} controls={controls} className="max-h-[768px]"></video>
+        </div>
     );
 }
 
 export const video = {
     render: "Video",
-    attributes: { url: { type: String } }
+    attributes: { src: { type: String } }
 };
