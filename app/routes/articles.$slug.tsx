b57f3394f3c8e1562055aa9ecf6e22130a8c3e9a
Wei Yen Lee <hello@weiyen.net>
2024-11-22 17:43:48 +1100

    Fix paths

--- app/routes/articles.$slug.tsx
+++ app/routes/articles.$slug.tsx
@@ -59,9 +59,9 @@ export function meta({ data }: { data: LoaderData }) {
   return [
     { title: `${article.title}` },
     { name: "description", content: article.description },
-    { property: "og:image", content: `articles/${article.slug}/oggraph.png` },
+    { property: "og:image", content: `${article.slug}/oggraph.png` },
     { name: "twitter:card", content: "summary_large_image" },
-    { name: "twitter:image", content: `articles/${article.slug}/oggraph.png` },
+    { name: "twitter:image", content: `${article.slug}/oggraph.png` },
   ];
 }
 
