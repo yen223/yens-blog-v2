cc79c2156b0e01f392ef9cee2a98caba917f3e84
Wei Yen Lee <hello@weiyen.net>
2024-12-05 23:38:04 +1100

    Improve contrast on footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -31,7 +31,7 @@ export function Footer() {
                 <NavLink href="/articles">Articles</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
               </div>
-              <p className="text-sm text-zinc-400 dark:text-zinc-500">
+              <p className="text-sm text-zinc-600 dark:text-zinc-400">
                 &copy; {new Date().getFullYear()} Wei Yen Lee. All rights
                 reserved. This site is <a href="https://github.com/yen223/yens-blog-v2" className="text-teal-500 underline">open source</a>!
               </p>
